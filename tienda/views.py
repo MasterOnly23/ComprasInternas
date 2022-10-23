f0c19ad0bcdd030a425d2cdf41bb2905fdf4a5d3
Pipe Daza <pipedaza23@gmail.com>
2022-10-23 15:47:07 -0300

    pag base

--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'productos.html')
